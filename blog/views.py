1f2c179349122e79d1d9322dbbcec64e54ea1a48
Johannes <mail@j23n.com>
2026-01-22 14:05:58 +0000

    add constants in templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
 from blog.models import Post, Tag
 
 
@@ -6,18 +7,18 @@ def home(request):
     posts = Post.objects.filter(namespace__name="public")
     tags = Tag.objects.filter(namespace__name="public")
 
-    return render(request, "home.html", {"posts": posts, "tags": tags})
+    return render(request, "home.html", {"posts": posts, "tags": tags, "CONSTANTS": settings.CONSTANTS})
 
 
 def post_list(request, namespace):
     posts = Post.objects.filter(namespace__name=namespace)
-    return render(request, "post-list.html", {"posts": posts})
+    return render(request, "post-list.html", {"posts": posts, "CONSTANTS": settings.CONSTANTS})
 
 
 def post_detail(request, namespace, year, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
 
-    return render(request, "post-detail.html", {"post": post})
+    return render(request, "post-detail.html", {"post": post, "CONSTANTS": settings.CONSTANTS})
 
 
 def tag_detail(request, namespace, name):
@@ -25,7 +26,7 @@ def tag_detail(request, namespace, name):
 
     posts = Post.objects.filter(tags__name=tag)
 
-    return render(request, "tag-detail.html", {"posts": posts, "tag": tag})
+    return render(request, "tag-detail.html", {"posts": posts, "tag": tag, "CONSTANTS": settings.CONSTANTS})
 
 
 def about(request):
