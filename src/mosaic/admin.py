350647cb101dd6b114632e057402acd5a3733bd3
Johannes <mail@j23n.com>
2026-02-03 12:51:46 +0100

    make post content textfield larger

--- src/mosaic/admin.py
+++ src/mosaic/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django import forms
 from mosaic.models import Post, Tag
 
 
@@ -6,6 +8,10 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = ["created_at"]
     list_display = ["title", "published_at", "namespace", "is_published", "created_at"]
     list_filter = ["is_published", "namespace", "tags", "published_at"]
+
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'rows':'20', 'style':'max-height: none; width: 100%'})},
+    }
 
 
 class TagAdmin(admin.ModelAdmin):
